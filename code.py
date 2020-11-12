67f34bd3db4dd08fb381cf1c6ef3f73bb745efba
luitelshreejal <luitelshreejal@gmail.com>
2020-11-11 17:41:31 -0800

    first test

--- code.py
+++ code.py
@@ -18,3 +18,38 @@ chan = AnalogIn(mcp, MCP.P0)
 
 print('Raw ADC Value: ', chan.value)
 print('ADC Voltage: ' + str(chan.voltage) + 'V')
+
+#We need to receive the output from the raspberry pi. analogRead value. chan.value
+
+
+
+#Convert the analog signal to a voltage. 
+
+
+voltage = chan.value*(5000/1024)
+
+#Convert the voltage to a PPM read. 
+
+#only if the voltage is larger than 400. 
+
+array = []
+
+for i in range(200)
+    if voltage < 400:
+        array.append("preheating")
+    else:
+        voltageDifference = voltage - 400
+        concentration = voltageDifference*50/16
+
+
+
+
+
+# int voltage_diference=voltage-400;
+# float concentration=voltage_diference*50.0/16.0;
+
+#create a pandas dataframe and append it on the dataframe. 
+
+#delay it 1000. 
+
+
